894d7c18e7ef992cd76dfaa4577e52d05205be77
Guilherme Vieira <guilhermevdm@gmail.com>
2019-02-03 22:51:51 -0300

    Added the core app, the custom user model that uses email instead of username as the identifier, and created a helper function for creating new user. Also that was implemented using TDD, so it also has a test for that helper function

---
+++ core/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        Test if creating a new user with email is successful
+        """
+
+        email = 'test@gambs.com'
+        password = 'testpassword123'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
